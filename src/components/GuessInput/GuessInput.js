bf3bc3b21e87ce5ce35a10a95554bdd0fa29c69d
Michael <mick_1402@hotmail.com>
2023-03-08 20:52:25 -0500

    Added input form for guesser

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
-function GuessInput() {
-  return <div />;
-}
+const GuessInput = ({ submitGuess }) => {
+  const [guess, setGuess] = useState("");
+
+  const capitalize = (str) => str.toUpperCase();
+
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(e) => {
+        e.preventDefault();
+        submitGuess(guess);
+        setGuess("");
+      }}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        required
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        type="text"
+        value={guess}
+        onChange={(evt) => {
+          setGuess(capitalize(evt.target.value));
+        }}
+      />
+    </form>
+  );
+};
 
 export default GuessInput;
